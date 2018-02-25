155fd33c02449ba515730a7f067c66547c8f2080
Malt <bestellungen@hoffmann-mail.eu>
2018-02-25 17:13:50 +0100

    refactoring of mapgenerator etc

--- SIZE/size.py
+++ SIZE/size.py
@@ -1,6 +1,6 @@
 class Size:
     """ Object size, specifiy either height and width, or square """
-    def __init__(self, height=1, width=1, square=1):
+    def __init__(self, height=1, width=1):
         self.height = height
         self.width = width
-        self.square = square
+        self.square = height * width
--- config.py
+++ config.py
@@ -3,3 +3,11 @@ RENDERAREAHEIGHT = 500
 MAPGRANULARITY = 1 # in px
 
 ITERATIONSPERSECOND = 60
+
+GENERATIONSTYLE = "Plain ground"
+
+terrain_types = {
+"GRASS": {"colour": [127, 255, 127], "solid":True},
+"AIR": {"colour": [127, 127, 255], "solid": False},
+"CLOUD": {"colour": [220, 220, 255], "solid": False}
+}
