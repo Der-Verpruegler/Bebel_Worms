7d8c1f132236ef6c988dde0cb118bad81e87d7f2
Malt <bestellungen@hoffmann-mail.eu>
2018-02-27 17:40:29 +0100

    initial branch commit

---
+++ WORM/worm.py
@@ -0,0 +1,20 @@
+import config
+
+class WORM():
+    def __init__(self):
+        pass
+        
+    
+    def spawn(self):
+        pass
+        
+    
+    def get_hitbox(self):
+        pass
+        
+        
+    def move(self, direction):
+        config.RENDERAREAHEIGHT
+        config.RENDERAREAWIDTH
+        
+        
